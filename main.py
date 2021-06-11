4fe869a7c75e93692e7f49587b273a390f34e8bf
Glenn de la Rie <glenndelarieo5@gmail.com>
2021-06-11 10:01:52 +0200

    alles is toegevoegd

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from distutils.version import LooseVersion
+import warnings
+import tensorflow as tf
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Check TensorFlow Version
+    assert LooseVersion(tf.__version__) >= LooseVersion(
+        '1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+    print('TensorFlow Version: {}'.format(tf.__version__))
+
+    # Check for a GPU
+    if not tf.test.gpu_device_name():
+        warnings.warn('No GPU found. Please ensure you have installed TensorFlow correctly')
+    else:
+        print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
+
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
